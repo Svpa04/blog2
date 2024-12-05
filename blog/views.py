1bb85dcb61bfb6370dcf16cfcd9bc200ac8393dd
Svpa04 <sofi04122001@gmail.com>
2024-12-05 18:04:05 -0400

    publication new done

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from .models import Publication
 # Create your views here.
 
@@ -7,4 +8,7 @@ class HomePageView(ListView):
     model = Publication
     template_name = "publication_list.html"
 
-
+class PublicationCreateView(CreateView):
+    model = Publication
+    template_name = "publication_new.html"
+    fields = ["title", "author", "body"]
